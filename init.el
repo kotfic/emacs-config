acfb28d8d1a85f14d64557fa4308461510422e4c
Christopher Kotfila <kotfic@gmail.com>
2015-07-21 09:24:13 -0400

    Fixes

--- init.el
+++ init.el
@@ -92,36 +92,42 @@
 
 (use-package helm
   :commands helm-mode
-  :bind  (("C-c h" . helm-command-prefix)
+  :bind  (("C-h a" . helm-apropos)
 	  ("C-c h g" . helm-google-suggest)
 	  ("C-c h o" . helm-occur)
 	  ("C-c h SPC" . helm-all-mark-rings)
 	  ("M-x" . helm-M-x)
 	  ("M-y" . helm-show-kill-ring)
 	  ("C-x b" . helm-mini)
-	  ("C-x C-f" . helm-find-files))
-  :config
-  (helm-mode 1)
-  (require 'helm-config)
+	  ("C-x C-f" . helm-find-files)
+)
+  :init
+  (progn
+    (helm-mode 1)
+;  (global-set-key (kbd "C-c h") 'helm-command-prefix)
+    (require 'helm-config)
+
+  
 
   ;; The default "C-x c" is quite close to "C-x C-c", which quits Emacs.
   ;; Changed to "C-c h". Note: We must set "C-c h" globally, because we
   ;; cannot change `helm-command-prefix-key' once `helm-config' is loaded.
 
-  (global-unset-key (kbd "C-x c"))
+  ;(global-unset-key (kbd "C-x c"))
 
-  (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to run persistent action
-  (define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB works in terminal
-  (define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
+    (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to run persistent action
+    (define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB works in terminal
+    (define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
+    
+    (when (executable-find "curl")
+      (setq helm-google-suggest-use-curl-p t))
 
-  (when (executable-find "curl")
-    (setq helm-google-suggest-use-curl-p t))
-
-  (setq helm-split-window-in-side-p           t ; open helm buffer inside current window, not occupy whole other window
-	helm-move-to-line-cycle-in-source     t ; move to end or beginning of source when reaching top or bottom of source.
-	helm-ff-search-library-in-sexp        t ; search for library in `require' and `declare-function' sexp.
-	helm-scroll-amount                    8 ; scroll 8 lines other window using M-<next>/M-<prior>
-	helm-ff-file-name-history-use-recentf t)
+    (setq helm-split-window-in-side-p           t ; open helm buffer inside current window, not occupy whole other window
+	  helm-move-to-line-cycle-in-source     t ; move to end or beginning of source when reaching top or bottom of source.
+	  helm-ff-search-library-in-sexp        t ; search for library in `require' and `declare-function' sexp.
+	  helm-scroll-amount                    8 ; scroll 8 lines other window using M-<next>/M-<prior>
+	  helm-ff-file-name-history-use-recentf t)
+    )
   )
 
 (use-package projectile

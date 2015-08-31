7e2ac05896c645783aebe5b727d8671ce4cd9427
Christopher Kotfila <chris.kotfila@kitware.com>
2015-08-31 09:07:44 -0400

    First attempt at defproject macro

--- lib/projects.el
+++ lib/projects.el
@@ -6,6 +6,64 @@
 ;;; Code:
 
 ;; We condiser these to be safe!
+
+
+(defun filter-plist (fn plist)
+  (let ((pl plist)
+        (vals ()))
+    (while pl
+      (push (funcall fn (car pl) (cadr pl)) vals)
+      (setq pl (cddr pl)))
+    (delq nil(nreverse vals))))
+
+(defun ismode? (symbol)
+  (or (s-contains? "-mode" (symbol-name symbol))
+      (equal ":nil" (symbol-name symbol))))
+
+(defun get-dir-locals (args)
+  (filter-plist (lambda (key val)
+		  (when (ismode? key)
+		    (cons key val))) args))
+
+(defun eval-dir-locals (args)
+  ;; for each mode and its var/value list
+  (-map (lambda (mode_args)
+	  (let ((mode (car mode_args))
+		(body (cdr mode_args)))
+	    ;; for each var and its value
+	    (cons mode (-map (lambda (var_val)
+			       (let ((var (car var_val))
+				     (val (cdr var_val)))
+				 ;; if first element is 'eval pass through
+				 ;; otherwise evaluate the cdr
+				 (if (eq var 'eval)				     
+				     var_val
+				   (cons var (eval val))))
+			       ) body))))
+	args))
+
+(defmacro defproject(project-name &rest args)
+  (declare (indent 1))
+  (let* ((project-name-symbol (if (stringp project-name)
+                                  (intern project-name)
+                                project-name))
+	 ))
+  
+  `(let* ((project-path ,(plist-get args :path))
+	  ,@(plist-get args :vars)
+	  (dir-locals (eval-dir-locals ,(get-dir-locals args))))
+     (when dir-locals
+       (-map (lambda(class-vars-list)
+	       (-map (lambda(class-var)
+		       (add-to-list 'safe-local-variable-values class-var))
+		     (cdr class-vars-list))) dir-locals)
+
+       (dir-locals-set-class-variables (quote ,project-name)
+				       dir-locals)
+       (if (file-exists-p project-path)
+	   (dir-locals-set-directory-class project-path (quote ,project-name))))))
+
+
 (mapc (lambda (e)
 	(add-to-list 'safe-local-variable-values e))
       '(

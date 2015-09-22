3f49923a58de99a6762d4d9c71827094be51fe27
Christopher Kotfila <chris.kotfila@kitware.com>
2015-09-22 19:16:51 -0400

    Fix agenda file so it doesn't contain classes

--- lib/org-meta.el
+++ lib/org-meta.el
@@ -1,19 +1,14 @@
-
-(defhydra hydra-comps-tagger
+(defhydra hydra-grant-tagger
   (:color pink
 	  :post hydra-org-tagger/body)
-  "Comps tagger"
-  ("0" (org-toggle-tag "cinf720") "cinf72(0)")
-  ("1" (org-toggle-tag "cinf721") "cinf72(1)")
-  ("2" (org-toggle-tag "cinf722") "cinf72(2)")
-  ("3" (org-toggle-tag "cinf723") "cinf72(3)")
-  ("4" (org-toggle-tag "cinf724") "cinf72(4)")
-  ("g" (org-toggle-tag "cgog500") "c(g)og500")
-  ("f" (org-toggle-tag "cframework") "c(f)ramework")
-  ("c" (org-toggle-tag "comps") "(c)omps")
-  ("q" (org-toggle-tag "cquote") "c(q)uote")
-
+  "Grants tagger"
+  ("t" (org-toggle-tag "term") "(t)erm")
+  ("p" (org-toggle-tag "paper") "(p)aper")
+  ("q" (org-toggle-tag "question") "(q)uestion")
+  ("s" (org-toggle-tag "story") "(s)tory")
+  ("f" (org-toggle-tag "followup") "(f)ollowup")
   ("<return>" nil "quit" :exit t))
+
 
 (define-key
   org-mode-map
@@ -21,5 +16,5 @@
   (defhydra hydra-org-tagger
     (:color blue)
     "Org-mode tagger"
-    ("c" hydra-comps-tagger/body "(c)omps" :color blue)
+    ("g" hydra-grant-tagger/body "(g)rants" :color blue)
     ("<return>" nil "quit" :exit t)))

23402e04d9368799464f4df1e3150982932069f7
Christopher Kotfila <chris.kotfila@kitware.com>
2016-10-18 15:54:23 -0400

    Include pkgbuild-mode

--- custom.el
+++ custom.el
@@ -29,7 +29,7 @@
      (:name "kitware" :query "path:kitware/**"))))
  '(package-selected-packages
    (quote
-    (yaml-mode weechat virtualenvwrapper use-package twittering-mode stylus-mode sphinx-doc scala-mode2 sauron rainbow-mode prodigy powerline php-mode ox-reveal org-trello org-plus-contrib org-pdfview org-bullets noflet markdown-mode magit link-hint js2-refactor jedi jade-mode hydra helm-swoop helm-projectile helm-descbinds helm-dash helm-ag guide-key git-timemachine flycheck ensime elfeed-org defproject company-tern color-theme-sanityinc-tomorrow color-theme-monokai cmake-mode badger-theme)))
+    (pkgbuild-mode yaml-mode weechat virtualenvwrapper use-package twittering-mode stylus-mode sphinx-doc scala-mode2 sauron rainbow-mode prodigy powerline php-mode ox-reveal org-trello org-plus-contrib org-pdfview org-bullets noflet markdown-mode magit link-hint js2-refactor jedi jade-mode hydra helm-swoop helm-projectile helm-descbinds helm-dash helm-ag guide-key git-timemachine flycheck ensime elfeed-org defproject company-tern color-theme-sanityinc-tomorrow color-theme-monokai cmake-mode badger-theme)))
  '(powerline-color1 "#222912")
  '(powerline-color2 "#09150F")
  '(safe-local-variable-values
